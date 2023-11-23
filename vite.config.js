5afa498ce48dc09423c949d262a08525da17e55f
isaul19 <saulhola348@gmail.com>
2023-11-23 11:37:52 -0500

    add base github

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://isaul19.github.io/test-github-actions/",
 });
