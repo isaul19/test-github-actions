07887fb3f7c9462c774be0d200102502a5ebbaa2
isaul19 <saulhola348@gmail.com>
2023-11-23 11:47:39 -0500

    test router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+
 export const App = () => {
   return (
-    <div>
-      <h1>Index page version 2</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <h1>Index page</h1>
+                <Link to="/test">Ir a test</Link>
+              </>
+            }
+          />
+
+          <Route
+            path="test"
+            element={
+              <>
+                <h1>Test page</h1>
+                <Link to="/test">Ir a index</Link>
+              </>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
